dccea6615d33f78833cc47e1a422a9fcf9f290e7
sm-remal <remal.mozzem@gmail.com>
2025-11-08 11:23:00 +0600

    server side done

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const client = new MongoClient(uri, {
 });
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
         // MongoDB Collection connect
 
@@ -209,7 +209,7 @@ async function run() {
         })
 
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
 
